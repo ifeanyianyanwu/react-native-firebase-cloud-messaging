1005ab0667bdc6376f697f93bf978a41c394ed93
IFEANYI ANYANWU <anyanwuifeanyi001@gmail.com>
2024-02-02 20:23:34 +0100

    init

--- App.js
+++ App.js
@@ -1,10 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import messaging from "@react-native-firebase/messaging";
+import { Alert } from "react-native";
+import { AppRegistry } from "react-native";
+import Button from "./components/button";
 
 export default function App() {
+  const requestUserPermission = async () => {
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+    if (enabled) {
+      console.log("Authorization status:", authStatus);
+    }
+  };
+
+  useEffect(() => {
+    if (requestUserPermission()) {
+      messaging()
+        .getToken()
+        .then((token) => {
+          console.log(token);
+        });
+    } else {
+      console.log("Failed to get token");
+    }
+
+    const unsubscribe = messaging().onMessage(async (remoteMessage) => {
+      Alert.alert("A new FCM message arrived!", JSON.stringify(remoteMessage));
+    });
+
+    return unsubscribe;
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button
+        label={"test button"}
+        onPress={() =>
+          messaging()
+            .subscribeToTopic("weather")
+            .then(() => alert("Subscribed to topic!"))
+        }
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +54,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
+
+// Register background handler
+messaging().setBackgroundMessageHandler(async (remoteMessage) => {
+  console.log("Message handled in the background!", remoteMessage);
+});
+
+AppRegistry.registerComponent("app", () => App);
+
+// messaging()
+//   .subscribeToTopic('weather')
+//   .then(() => console.log('Subscribed to topic!'));
---
+++ components/button.js
@@ -0,0 +1,39 @@
+import { StyleSheet, View, Pressable, Text } from "react-native";
+
+export default function Button({ label, onPress }) {
+  return (
+    <View style={styles.buttonContainer}>
+      <Pressable style={styles.button} onPress={onPress}>
+        <Text style={styles.buttonLabel}>{label}</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    backgroundColor: "#fff",
+    width: 200,
+    height: 40,
+    borderRadius: 10,
+    marginHorizontal: 20,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 3,
+  },
+  button: {
+    borderRadius: 10,
+    width: "100%",
+    height: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+  },
+  buttonIcon: {
+    paddingRight: 8,
+  },
+  buttonLabel: {
+    color: "#000",
+    fontSize: 16,
+  },
+});
